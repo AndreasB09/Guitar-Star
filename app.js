31a6769ee69981cc94894561011b92f60639a80b
andreasb09 <andreasb09.kodehode@gmail.com>
2024-01-15 14:32:04 +0100

    start bg animation

--- app.js
+++ app.js
@@ -3,3 +3,26 @@
 //create buttons and function
 
 //add mouseclick and keypress listeners
+
+//background stars
+function createFallingStar() {
+    const fallingStar = document.createElement('div');
+    fallingStar.classList.add('falling-star')
+    document.getElementById('container').appendChild(fallingStar);
+
+    const randomStar = Math.random() * (window.innerWidth - 20);
+    fallingStar.style.left = `${randomStar}px`;
+
+    const randomDelay = Math.random() * 2000;
+
+    setTimeout(() => {
+      fallingStar.style.animationDelay = `-${randomDelay}ms`;
+      fallingStar.style.animationPlayState = "running";
+    }, randomDelay);
+}
+
+function startAnimation() {
+    setInterval(createFallingStar, 2000);
+}
+
+window.onload = startAnimation;
